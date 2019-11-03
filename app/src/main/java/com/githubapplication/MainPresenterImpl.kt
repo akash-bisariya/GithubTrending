5e9bf1e4acd4015f45261e9e399922634058121d
Akash Bisariya <akashb@one.com>
2019-11-03 10:31:37 +0530

    Added mainpresenter

--- app/src/main/java/com/githubapplication/MainPresenterImpl.kt
+++ app/src/main/java/com/githubapplication/MainPresenterImpl.kt
@@ -1,4 +1,33 @@
 package com.githubapplication
 
-class MainPresenterImpl {
+import com.TrendingRepositories
+
+class MainPresenterImpl(private val mainView: MainContract.MainView, private val mainInteractor: MainContract.MainInteractor) : MainContract.MainPresenter, MainContract.MainInteractor.OnFinishedListener {
+
+
+    override fun requestDataFromServer(language: String) {
+
+        mainView.showProgress()
+        mainInteractor.getTrendingRepositories(this,language)
+
+    }
+
+    override fun onDestroy() {
+
+    }
+
+    override fun onSuccess(trendingRepositories: TrendingRepositories) {
+
+        mainView.hideProgress()
+        mainView.setData(trendingRepositories)
+
+    }
+
+    override fun onFailure() {
+
+        mainView.hideProgress()
+
+    }
+
+
 }
