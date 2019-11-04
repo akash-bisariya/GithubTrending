36e479cde10120c3aa116a3c4ca8d58b46b2470c
Akash Bisariya <akashb@one.com>
2019-11-04 14:21:53 +0530

    chnaged row-layout and spinner dataset

---
+++ app/src/test/java/com/githubapplication/mainactivity/MainInteractorImplTest.kt
@@ -0,0 +1,15 @@
+package com.githubapplication.mainactivity
+
+import org.junit.Before
+import org.junit.Test
+
+class MainInteractorImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getTrendingRepositories() {
+    }
+}
